3741b9e27b9213836245996a876f1ad2df23a6f6
turulomio <turulomio@yahoo.es>
2022-08-13 11:21:14 +0200

    ssh_telegram_manager-0.1.1

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ssh_telegram_manager
 Telegram bot to launch ssh on demand
+
+## Changelog
+### 0.1.1 (2022-08-13)
+- Basic functionality
--- ssh_telegram_manager/__init__.py
+++ ssh_telegram_manager/__init__.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-__version__ = '0.1.0'
-__versiondatetime__=datetime(2022, 8, 13, 11, 1)
+__version__ = '0.1.1'
+__versiondatetime__=datetime(2022, 8, 13, 11, 20)
 __versiondate__=__versiondatetime__.date()
 
