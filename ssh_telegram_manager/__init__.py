26b31969d8df31967badd19bbe22f41fe4e9f148
turulomio <turulomio@yahoo.es>
2022-08-13 11:01:37 +0200

    ssh_telegram_manager-0.1.0

--- ssh_telegram_manager/__init__.py
+++ ssh_telegram_manager/__init__.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-__version__ = '0.3.0'
-__versiondatetime__=datetime(2022, 6, 7, 8, 29)
+__version__ = '0.1.0'
+__versiondatetime__=datetime(2022, 8, 13, 11, 1)
 __versiondate__=__versiondatetime__.date()
 
