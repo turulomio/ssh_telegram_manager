bd65a955d164c2cf18cb061d53e532fc0cf35b46
turulomio <turulomio@yahoo.es>
2023-12-01 23:46:58 +0100

    Migrated setup.py to pyproject.toml with poetry (#8)
    * Adding files for poetry and poethepoet
    
    * Changing project structure
    
    * poe translate works
    
    * Removed reusing

---
+++ ssh_telegram_manager/poethepoet.py
@@ -0,0 +1,36 @@
+from ssh_telegram_manager import __version__
+from os import system
+
+def pytest():
+    system("pytest")
+    
+def coverage():
+    system("coverage run --omit='*/reusing/*,*uno.py' -m pytest && coverage report && coverage html")
+
+def translate():
+        system("xgettext -L Python --no-wrap --no-location --from-code='UTF-8' -o ssh_telegram_manager/locale/ssh_telegram_manager.pot  ssh_telegram_manager/*.py ssh_telegram_manager/reusing/*.py ")
+        system("msgmerge -N --no-wrap -U ssh_telegram_manager/locale/es.po ssh_telegram_manager/locale/ssh_telegram_manager.pot")
+        system("msgfmt -cv -o ssh_telegram_manager/locale/es/LC_MESSAGES/ssh_telegram_manager.mo ssh_telegram_manager/locale/es.po")
+        system("msgfmt -cv -o ssh_telegram_manager/locale/en/LC_MESSAGES/ssh_telegram_manager.mo ssh_telegram_manager/locale/en.po")
+
+def release():
+    print("""
+    Nueva versión:
+  * Cambiar la versión y la fecha en commons.py
+  * Cambiar la versión en pyproject.toml
+  * Modificar el Changelog en README
+  * poe coverage
+  * poe translate
+  * linguist
+  * poe translate
+  * poe documentation
+  * git commit -a -m 'ssh_telegram_manager-{0}'
+  * git push
+  * Hacer un nuevo tag en GitHub
+  * poetry build
+  * poetry publish --username --password  
+  * Crea un nuevo ebuild de ssh_telegram_manager Gentoo con la nueva versión
+  * Subelo al repositorio del portage
+
+""".format(__version__))
+
