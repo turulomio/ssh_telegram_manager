1265601eb5e27ff46b53766fed48abf72e7f0572
turulomio <turulomio@yahoo.es>
2023-12-01 23:45:05 +0100

    Removed reusing

--- ssh_telegram_manager/poethepoet.py
+++ ssh_telegram_manager/poethepoet.py
@@ -1,8 +1,5 @@
-from ssh_telegram_manager.reusing.github import download_from_github
 from ssh_telegram_manager import __version__
 from os import system
-from sys import argv
-
 
 def pytest():
     system("pytest")
@@ -10,52 +7,14 @@ def pytest():
 def coverage():
     system("coverage run --omit='*/reusing/*,*uno.py' -m pytest && coverage report && coverage html")
 
-
-def reusing():
-    """
-        Actualiza directorio reusing
-        poe reusing
-        poe reusing --local
-    """   
-    local=False
-    if len(argv)==2 and argv[1]=="--local":
-        local=True
-        print("Update code in local without downloading was selected with --local")
-    if local==False:
-        download_from_github('turulomio','reusingcode','python/github.py', 'ssh_telegram_manager/reusing/')
-        download_from_github('turulomio','reusingcode','python/file_functions.py', 'ssh_telegram_manager/reusing/')
-
 def translate():
         system("xgettext -L Python --no-wrap --no-location --from-code='UTF-8' -o ssh_telegram_manager/locale/ssh_telegram_manager.pot  ssh_telegram_manager/*.py ssh_telegram_manager/reusing/*.py ")
         system("msgmerge -N --no-wrap -U ssh_telegram_manager/locale/es.po ssh_telegram_manager/locale/ssh_telegram_manager.pot")
         system("msgfmt -cv -o ssh_telegram_manager/locale/es/LC_MESSAGES/ssh_telegram_manager.mo ssh_telegram_manager/locale/es.po")
         system("msgfmt -cv -o ssh_telegram_manager/locale/en/LC_MESSAGES/ssh_telegram_manager.mo ssh_telegram_manager/locale/en.po")
 
-
-
-
 def release():
     print("""
-    
-            Nueva versión:
-  * Cambiar la versión y la fecha en __init__.py
-  * Modificar el Changelog en README
-  * python setup.py translate
-  * linguist
-  * python setup.py translate
-  * python setup.py uninstall; python setup.py install
-  * python setup.py doxygen
-  * git commit -a -m 'ssh_telegram_manager-{0}'
-  * git push
-  * Hacer un nuevo tag en GitHub
-  * python setup.py sdist
-  * twine upload dist/ssh_telegram_manager-{0}.tar.gz 
-  * python setup.py uninstall
-  * Crea un nuevo ebuild de ssh_telegram_manager Gentoo con la nueva versión
-  * Subelo al repositorio del portage
-
-    
-    
     Nueva versión:
   * Cambiar la versión y la fecha en commons.py
   * Cambiar la versión en pyproject.toml
